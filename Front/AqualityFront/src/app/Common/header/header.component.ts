8da25cc7172b54c45a69a774f10ab82c8d1269d1
alexiaambrizz <alexaramirez12300@gmail.com>
2024-01-06 01:28:22 -0600

    links

--- Front/AqualityFront/src/app/Common/header/header.component.ts
+++ Front/AqualityFront/src/app/Common/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { NavegationComponent } from './navegation/navegation/navegation.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NavegationComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
 }
--- Front/AqualityFront/src/app/Common/header/navegation/navegation/navegation.component.ts
+++ Front/AqualityFront/src/app/Common/header/navegation/navegation/navegation.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink,RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navegation',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './navegation.component.html',
   styleUrl: './navegation.component.scss'
 })
 export class NavegationComponent {
-
+  menu=[
+    {titulo: "Inicio", url: "/start"},
+    {titulo: "Conócenos", url: "/about"},
+    {titulo: "Ubicación", url: "/location"},
+    {titulo: "Tiendas", url: "/stores"},
+    {titulo: "Productos", url: "/products"},
+  ];
 }
+
