781e93cf8b26239b9c97b6d2027c8c2e875675f7
Dern965 <dern965@hotmail.com>
2024-01-06 16:25:34 -0600

    Aun falta el formulario

--- Back/ApiGateway/obj/Debug/net6.0/ApiGateway.AssemblyInfo.cs
+++ Back/ApiGateway/obj/Debug/net6.0/ApiGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da25cc7172b54c45a69a774f10ab82c8d1269d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+250d11200291f35ecb2760b9e76391e211e946a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
