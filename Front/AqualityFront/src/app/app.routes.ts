07534c5ae3162fddd9dc969da919aba30763a2f4
jxrgxncrxx <jurgen.qetu@gmail.com>
2024-01-06 19:15:32 -0600

    Diseno en pages

--- Front/AqualityFront/src/app/app.routes.ts
+++ Front/AqualityFront/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { StartComponent } from './pages/start/start/start.component';
+import { Component } from '@angular/core';
+import { AboutComponent } from './pages/about/about/about.component';
+import { LocationComponent } from './pages/location/location/location.component';
+import { ProductsComponent } from './pages/products/products/products.component';
+import { StoresComponent } from './pages/stores/stores/stores.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component:StartComponent},
+    {path:"start", component:StartComponent},
+    {path:"about", component:AboutComponent},
+    {path:"location", component:LocationComponent},
+    {path:"products", component:ProductsComponent},
+    {path:"stores", component:StoresComponent}
+];
